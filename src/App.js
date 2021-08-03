23c1374fa7db7e9c78a50b8801f687a09fbffe08
Alexander <alexpoidenko@gmail.com>
2021-08-03 17:50:44 +0200

    Maquetacion

--- src/App.js
+++ src/App.js
@@ -3,30 +3,45 @@ import './App.css';
 
 function App() {
   return (
-    <header>
-      <div className="menu-container">
-        <img src="https://www.neoland.es/hubfs/favicon%20neoland-02-02-02.png"></img>
-        <p class="version">Version 1.0</p>
-      </div>
-     
+    <div className='container'>
+      <header>
+        <div className="menu-container">
+          <img src="https://www.neoland.es/hubfs/favicon%20neoland-02-02-02.png"></img>
+          <p class="version">Version 1.0</p>
+        </div>
+      </header>
+
 
       <div>
-      <p>Version 1.0</p>
+        <p>Version 1.0</p>
       </div>
-        <div className="input-container">
-      <p>Updated on 12 Apr</p>
+      <div className="input-container">
+        <p>Updated on 12 Apr</p>
         <input className="input" placeholder="🔎filter cards"></input>
       </div>
 
-      <div className="toDoList-container">
-        <p className="task-number">1</p>
-        <p className="todo">To do</p>
-        <button className="add-button">+</button>
+
+      <div className='column-container'>
+        <div className='todo-column column'>
+          <div className="toDoList-container header-container">
+            <div className='toDo-header'>
+              <p className="task-number">1</p>
+              <p className="todo">To do</p>
+            </div>
+            <button className="add-button">+</button>
+          </div>
+        </div>
+        <div className='inprogress-column column'>
+
+        </div>
+        <div className='done-column column'></div>
       </div>
 
-      </header>
 
-   
+
+
+
+    </div>
   );
 }
 
