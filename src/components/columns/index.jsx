34f0bf15fd1ce11645765f883eac9084aaf280d9
VirginiaPoglonig <virginiapoglonig@gmail.com>
2021-08-07 16:59:58 +0200

    cambios-input

--- src/components/columns/index.jsx
+++ src/components/columns/index.jsx
@@ -7,7 +7,7 @@ function Columns(props) {
     return (
         <div className="columns_container">
             <Column columnName="To Do" numberOfCards='1' status="undone"></Column>
-            <Column columnName="In progress" numberOfCards='1' status="undone"></Column>
+            <Column columnName="In progress" numberOfCards='1' status="undone" ></Column>
             <Column columnName= "Done" numberOfCards='1' status="done" clearall={true}></Column>
         </div>
     )
